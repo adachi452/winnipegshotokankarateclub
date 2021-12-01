476e94ae6752e852d318ba3c6979b59b1ba36c25
adachi452 <83194816+adachi452@users.noreply.github.com>
2021-11-30 21:33:22 -0600

    Created a comment page for logged in users to comment on posts with

---
+++ comment.php
@@ -0,0 +1,98 @@
+<?php
+
+    session_start();
+
+    require('connect.php');
+
+    $query2 = "SELECT * FROM categories";
+
+        // A PDO::Statement is prepared from the query.
+        $statement2 = $db->prepare($query2);
+
+        // Execution on the DB server is delayed until we execute().
+        $statement2->execute();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>New Comment</title>
+    <link rel = "stylesheet" type = "text/css" href = "shotokanstyles.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <header>
+        <h1>Shotokan Karate</h1>
+    </header>
+    <?php if(isset($_SESSION['login']) && $_SESSION['userlevel'] <= 2): ?>
+            <nav>
+        <ul>
+            <li><a href = "index.php">Home Page</a></li>
+            <li><a href = "news.php?categoryid=4">News</a></li>
+            <li><a href = "instructors.php">Instructors</a></li>
+            <li><a href = "logout.php">Logout</a></li>
+        </ul>
+    </nav>
+    <h2>Welcome, <?= $_SESSION['login'] . "!"?></h2>
+
+    <?php elseif (isset($_SESSION['login']) && $_SESSION['userlevel'] = 3) : ?>  
+                <nav>
+        <ul>
+            <li><a href = "admin.php">Admin</a></li>
+            <li><a href = "index.php">Home Page</a></li>
+            <li><a href = "news.php?categoryid=4">News</a></li>
+            <li><a href = "instructors.php">Instructors</a></li>
+            <li><a href = "logout.php">Logout</a></li>
+        </ul>
+    </nav>  
+        <h2>Welcome, <?= $_SESSION['login'] . "!"?></h2>
+
+    <?php else: ?>    
+    <nav>
+        <ul>
+            <li><a href = "index.php">Home Page</a></li>
+            <li><a href = "instructors.php">Instructors</a></li>
+            <li><a href = "login.php">Login</a></li>
+        </ul>
+    </nav>
+    <?php endif ?>
+    <div id="wrapper">
+        <div id="header">
+            <h1><a href="index.php">New Post</a></h1>
+        </div> 
+<ul id="menu">
+    <li><a href="news.php" >Home</a></li>
+    <li><a href="create.php" class='active'>New Post</a></li>
+</ul> 
+<div id="all_blogs">
+  <form action="insert.php" method="post">
+    <fieldset>
+      <legend>New Post</legend>
+      <p>
+        <label for="comment">Comment</label>
+        <textarea name="comment" id="comment" type="text" required="text" /></textarea>
+      </p>
+      <p>
+        <input type="submit" name="command" value='Comment' />
+      </p>
+    </fieldset>
+  </form>
+</div><br>
+
+<footer>
+    <div id="links2">
+        <a href="index.php" class = "links2"> Home | </a>
+        <a href="news.php?categoryid=4" class = "links2">News | </a>
+        <a href="instructors.php" class = "links2">Instructors </a>
+    </div>  
+    <div id="line2">
+        <p class = "line2"> Copyright © 2021 <a href="https://iskf.com/club-directory/pan-america/canada/" class="link3"> International Shotokan Karate Federation</a> </p>
+    </div>  
+    <div id="line3">
+        <p class = "line3"> Monica's Danz Gym Unit #4-25 Scurfield Blvd, Winnipeg, Manitoba, Canada</p>
+    </div>  
+</footer>
+    </div> 
+</body>
+</html>
