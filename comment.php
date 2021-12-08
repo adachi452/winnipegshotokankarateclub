5bab2d6bfc82e5a4abfde2d9b56f3b915b325b09
adachi452 <83194816+adachi452@users.noreply.github.com>
2021-12-07 23:38:29 -0600

    Added the code nessisary to make the CAPTCHA work on my comments page.

--- comment.php
+++ comment.php
@@ -4,22 +4,44 @@
 
     require('connect.php');
 
-    $query2 = "SELECT * FROM categories";
+    // SQL is written as a String.
+     $query = "SELECT * FROM blog WHERE postid = :id LIMIT 1";
 
-        // A PDO::Statement is prepared from the query.
-        $statement2 = $db->prepare($query2);
+     // A PDO::Statement is prepared from the query.
+     $statement = $db->prepare($query);
 
-        // Execution on the DB server is delayed until we execute().
-        $statement2->execute();
+     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+     // Execution on the DB server is delayed until we execute().
+      $statement->bindValue('id', $id, PDO::PARAM_INT);
+      $statement->execute(); 
+
+     // Fetch the row selected by primary key id.
+     $row = $statement->fetch();
+
+
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head> 
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>New Comment</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel = "stylesheet" type = "text/css" href = "shotokanstyles.css" />
     <link rel="stylesheet" type="text/css" href="styles.css" />
+    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+    <script type="text/javascript">
+
+
+ var refreshButton = document.querySelector(".refresh-captcha");
+refreshButton.onclick = function() {
+  document.querySelector(".captcha-image").src = 'captcha.php?' + Date.now();
+
+</script>
 </head>
 <body>
     <header>
@@ -62,23 +84,41 @@
             <h1><a href="index.php">New Post</a></h1>
         </div> 
 <ul id="menu">
-    <li><a href="news.php" >Home</a></li>
+    <li><a href="news.php?categoryid=4" >Home</a></li>
     <li><a href="create.php" class='active'>New Post</a></li>
 </ul> 
 <div id="all_blogs">
   <form action="insert.php" method="post">
     <fieldset>
-      <legend>New Post</legend>
+      <legend>Replying to...</legend>
+      <p><?= $row['Title'] ?></p>
+      <p><?= $row['Content'] ?></p>
+      <input type="hidden" name="id" value="<?= $row['postid'] ?>" />
       <p>
         <label for="comment">Comment</label>
         <textarea name="comment" id="comment" type="text" required="text" /></textarea>
       </p>
+      <input type="hidden" name="username" value="<?= $_SESSION['login'] ?>" />
+      <input type="hidden" name="userid" value="<?= $_SESSION['userid'] ?>" />
+      <input type="hidden" name="captcha" value="<?= $_SESSION['captcha_text'] ?>" />
+
+<div class="elem-group">
+    <label for="captcha">Please Enter the Captcha Text</label>
+    <img src="captcha.php" alt="CAPTCHA" class="captcha-image"><i class="fas fa-redo refresh-captcha"></i>
+    <br>
+    <input type="text" id="captcha" name="captcha_challenge" pattern="[A-Z]{6}">
+</div>
+
       <p>
         <input type="submit" name="command" value='Comment' />
       </p>
+
     </fieldset>
   </form>
 </div><br>
+
+<script>CKEDITOR.replace('comment');</script>
+
 
 <footer>
     <div id="links2">
